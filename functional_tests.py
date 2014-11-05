da432c1d203415c549769cb749975a9cf646ea70
Andrew Reading-Legh <reg@localhost.localdomain>
2014-11-05 19:33:00 +0000

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -18,17 +19,32 @@ class NewVisitorTest(unittest.TestCase):
 
         #Noticing the page title and header mentions to-do lists
         self.assertIn ('To-Do', self.browser.title)
-        self.fail ('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn ('To-Do', header_text)
 
         #Invited to enter a to-do item right away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+            )
 
-        #Types in "finish learning javascript"(To add to fullstack cv"
+        #Types in "finish learning javascript"(To add to fullstack cv)"
+        inputbox.send_keys('Finish learning Javascript')
 
         #When hitting enter page updates and now the page lists:
         #"1, Finish learning Javascript" as an item on the to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Finish learning Javascript' for row in rows)
+            )
 
         #There is still a text box inviting the user to enter another item
         #enters "use code to make a dynamic website"
+        self.fail('Finish the test!')
 
         #The page updates again and shows both items on the list
 
